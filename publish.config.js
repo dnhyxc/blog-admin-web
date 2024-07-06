e804ada740df7ef54134192fea2887d921797af0
dnhyxc <2640559435@qq.com>
2024-07-06 12:07:46 +0800

    feat: 增加自动发布

---
+++ publish.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  // 服务器配置
+  serverInfo: {
+    // 目标服务器IP
+    host: '101.43.50.15',
+    // 目标服务器用户名
+    username: 'root',
+    // 端口号
+    port: 22,
+  },
+  porjectInfo: {
+    blogAdminWeb: {
+      name: 'admin_html',
+      // dist 文件路径
+      localFilePath: '/Users/dnhyxc/Documents/code/blog-admin-web',
+      // 目标服务器项目文件路径
+      remoteFilePath: '/usr/local/nginx/html_admin',
+      isServer: false
+    }
+  }
+}
